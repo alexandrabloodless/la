519ba3e4382779adb5a49a3faf4514cddc212c9a
bloodless <bloodless@Beskr.(none)>
2013-12-02 18:28:45 +0400

    Two exersises was done

---
+++ session_3/print_settings.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+. ~/la/session_3/settings.conf
+echo -e "#########################################\n## Default login/password: $OS_USERNAME/$OS_PASSWORD\n## tenant: $OS_TENANT_NAME\n#########################################"
